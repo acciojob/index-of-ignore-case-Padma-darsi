48dd07179389af09c8ec2e15883af42cd832f538
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-11-30 22:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-// Convert both strings to lowercase for case-insensitive comparison
-  let lowerS1 = s1.toLowerCase();
-  let lowerS2 = s2.toLowerCase();
-  
-  // Use indexOf to find the index of the substring in the main string
-  return lowerS1.indexOf(lowerS2);
+function indexOfIgnoreCase(str: string, subStr: string): number {
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
 
+  // Return the index of the first occurrence of subStr in str, case-insensitively
+  return lowerStr.indexOf(lowerSubStr);
 }
 
 // Examples
@@ -14,7 +12,6 @@ console.log(indexOfIgnoreCase("Hello World", "world")); // Output: 6
 console.log(indexOfIgnoreCase("apple", "Ple")); // Output: 2
 console.log(indexOfIgnoreCase("test", "aaa")); // Output: -1
 
-}
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
