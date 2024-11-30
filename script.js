7a644852335fd9084da6a45c253ac85197e0b796
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-11-30 22:01:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+	function indexOfIgnoreCase(str, subStr) {
+  // Convert both the string and the substring to lowercase
+  let lowerStr = str.toLowerCase();
+  let lowerSubStr = subStr.toLowerCase();
+
+  // Use the indexOf method to find the first occurrence of subStr in str
+  return lowerStr.indexOf(lowerSubStr);
+}
+
+// Examples
+console.log(indexOfIgnoreCase("Hello World", "world")); // Output: 6
+console.log(indexOfIgnoreCase("apple", "Ple")); // Output: 2
+console.log(indexOfIgnoreCase("test", "aaa")); // Output: -1
+
 }
 
 // Please do not change the code below
